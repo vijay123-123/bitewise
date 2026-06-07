bad606a14dcecfa831321ce71fb0cd84f731d390
Vijay <vijayshankarganesh.m@gmail.com>
2026-06-07 11:25:56 +0530

    springboot default folder created

---
+++ backend/bitewise-backend/src/main/java/com/supreme/bitewise_backend/BitewiseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.supreme.bitewise_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitewiseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitewiseBackendApplication.class, args);
+	}
+
+}
---
+++ backend/bitewise-backend/src/test/java/com/supreme/bitewise_backend/BitewiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supreme.bitewise_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitewiseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
